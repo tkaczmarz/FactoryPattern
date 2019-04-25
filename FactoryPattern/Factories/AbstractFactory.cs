48efcde6b4b7e4520d0a8d8fa7ed21e54e58c577
tkaczmarz <kaczmarz.tomasz@gmail.com>
2019-04-25 22:52:06 +0200

    Added abstract factory example.

---
+++ FactoryPattern/Factories/AbstractFactory.cs
@@ -0,0 +1,56 @@
+﻿using FactoryPattern.Model;
+using FactoryPattern.Model.CPU;
+using FactoryPattern.Model.GPU;
+using FactoryPattern.Model.Monitors;
+
+namespace FactoryPattern.Factories
+{
+    public interface IAbstractFactory
+    {
+        CPU CreateCPU();
+        GPU CreateGPU();
+        Monitor CreateMonitor();
+    }
+
+    public class GamingFactory : IAbstractFactory
+    {
+        private static GamingFactory instance = null;
+        private static readonly object padlock = new object();
+
+        public static GamingFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new GamingFactory();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private GamingFactory() { }
+
+        public CPU CreateCPU()
+        {
+            return new Core_i7();
+        }
+
+        public GPU CreateGPU()
+        {
+            return new RTX2080();
+        }
+
+        public Monitor CreateMonitor()
+        {
+            return new Asus();
+        }
+    }
+}
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -22,6 +22,11 @@ namespace FactoryPattern
                 Console.WriteLine(SimpleFactory.Instance.CreateMonitor(type).GetSpecs());
             }
 
+            Console.WriteLine("ABSTRACT:");
+            Console.WriteLine(GamingFactory.Instance.CreateCPU().Benchmark());
+            Console.WriteLine(GamingFactory.Instance.CreateGPU().Benchmark());
+            Console.WriteLine(GamingFactory.Instance.CreateMonitor().GetSpecs());
+
             Console.ReadKey();
         }
     }
